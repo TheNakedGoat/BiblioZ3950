abe03f54ef98bcaba3ca6c9c5664b9c13d817b51
Alessandro D'Andrea <a.dandrea@gmail.com>
2024-07-03 12:48:14 +0200

    Create config.js

---
+++ biblioZ3950/assets/config.js
@@ -0,0 +1,33 @@
+{
+  "server": {
+    "host": "opac.sbn.it",
+    "port": 3950,
+    "database": "UNIMARC"
+  },
+  "search_mapping": {
+    "title": {
+      "use": 4,
+      "description": "Search by title"
+    },
+    "author": {
+      "use": 1003,
+      "description": "Search by author"
+    },
+    "isbn": {
+      "use": 7,
+      "description": "Search by ISBN"
+    },
+    "publication_year": {
+      "use": 31,
+      "description": "Search by publication year"
+    },
+    "publisher": {
+      "use": 1018,
+      "description": "Search by publisher"
+    },
+    "category": {
+      "use": 1032,
+      "description": "Search by category or subject"
+    }
+  }
+}
